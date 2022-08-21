634189be8ea1d00c4ed574b40847cdad6c4d3b02
Asher <ayang@safetrack.ai>
2022-08-21 15:15:01 -0400

    minor

--- apps/portfolio/src/pages/Resume/components/atomics/Spotlight.tsx
+++ apps/portfolio/src/pages/Resume/components/atomics/Spotlight.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 export default function Spotlight(props) {
-  const { keypoint } = props;
+  const { keypoint, ...rest } = props;
   return (
     <li className='mb-2'>
-      {keypoint ? <span className='font-bold'>{keypoint}:</span> : null}{' '}
+      {keypoint ? <span className='font-bold capitalize'>{keypoint}:</span> : null}{' '}
       {props.children}
     </li>
   );
